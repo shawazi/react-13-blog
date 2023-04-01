ecfbac90fec2172b84e511ccbb73e81c18d3699e
shawazi <shawazisonfire@gmail.com>
2023-04-01 18:41:47 -0400

    initialized

---
+++ src/components/WazFoot.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WazFoot = () => {
+  return (
+    <div>WazFoot</div>
+  )
+}
+
+export default WazFoot
---
+++ src/components/WazNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WazNav = () => {
+  return (
+    <div>WazNav</div>
+  )
+}
+
+export default WazNav
